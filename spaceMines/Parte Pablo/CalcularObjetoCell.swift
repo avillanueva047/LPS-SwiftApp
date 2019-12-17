4d433136bf761bcac40ea244c9a86891af3a58eb
Aula11 <aula11@a_11_07.aulastic2012.ual.es>
2019-12-17 18:28:40 +0100

    Credas carpetas para todos (en ellas crearemos las clases que nos corresponden a cada uno. Añadidas casi todas las vistas de la aplicación ( faltan las 6 últimas)

---
+++ spaceMines/Parte Pablo/CalcularObjetoCell.swift
@@ -0,0 +1,24 @@
+//
+//  CalcularObjetoCell.swift
+//  spaceMines
+//
+//  Created by Aula11 on 17/12/19.
+//  Copyright © 2019 ual. All rights reserved.
+//
+
+import UIKit
+
+class CalcularObjetoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
