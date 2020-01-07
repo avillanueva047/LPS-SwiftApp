8d4d618674518177b5ede8cc419bfb84b50bc37c
Aula11 <aula11@a_11_02.aulastic2012.ual.es>
2020-01-07 18:15:29 +0100

    Elementos incluidos en pantalla exploración

---
+++ spaceMines/PantallaInicio.h
@@ -0,0 +1,17 @@
+//
+//  PantallaInicio.h
+//  spaceMines
+//
+//  Created by Aula11 on 7/1/20.
+//  Copyright © 2020 ual. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PantallaInicio : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ spaceMines/PantallaInicioViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PantallaInicioViewController.swift
+//  spaceMines
+//
+//  Created by Aula11 on 7/1/20.
+//  Copyright © 2020 ual. All rights reserved.
+//
+
+import UIKit
+
+class PantallaInicioViewController: UIViewController {
+
+    @IBOutlet weak var logotipo: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "Imagen fondo LPS.jpg")
+        backgroundImage.contentMode =  UIView.ContentMode.scaleAspectFill
+        self.view.insertSubview(backgroundImage, at: 0)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
